1690372f1f55321eccee58746b0d994c5b43d940
Ali Cherry <32630359+alichry@users.noreply.github.com>
2025-04-22 01:01:58 +0100

    add playground to facilitate Berytus-X509-ext experiments

---
+++ playground/astro.config.mjs
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
